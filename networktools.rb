266a8df24ee920acf89dfdab19aa7b70157c5420
padulas <41273121+padulas@users.noreply.github.com>
2019-08-30 16:03:08 -0300

    321
    Signed-off-by: padulas <41273121+padulas@users.noreply.github.com>

--- networktools.rb
+++ networktools.rb
@@ -1,8 +1,15 @@
 #Ferramenta de checagem de host
 
 #Checagem de portas
+#Whois Resumido
+#
+#
 
 puts "Informe um domínio:"
 dominio = gets.chomp
 
-cmd system (whois #{dominio} |grep -E "changed|expires|provider|created|status")
+cmd = system (whois #{dominio} |grep -E "changed|expires|provider|created|status")
+
+
+
+
