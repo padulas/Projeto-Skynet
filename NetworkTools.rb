26bce99fd94ad4b809e828ec22726389ceb1067a
padulas <41273121+padulas@users.noreply.github.com>
2019-08-30 15:31:42 -0300

    Delete NetworkTools.rb

--- NetworkTools.rb
+++
@@ -1,8 +0,0 @@
-#Ferramenta de checagem de host
-
-#Checagem de portas
-
-puts "Informe um domínio:"
-dominio = gets.chomp
-
-cmd system (whois #{dominio} |grep -E "changed|expires|provider|created|status")
