477e3d32d451fb94a2c44a11f3856e7b23a5d0df
root <root@cpro43105.publiccloud.com.br>
2019-09-04 00:24:20 -0400

    Ferramentas de segurança atribuídas ao script

--- skynet.rb
+++ skynet.rb
@@ -1,7 +1,8 @@
-#Versão Inicial Skynet 2019 - Whois corrigido
+#Versão Inicial Skynet 2019
+#skynet.rb - Build 1.0
 
-#cat skynet.rb - Build 1.0
 #ruby 2.5.5p157 (2019-03-15 revision 67260) [x86_64-linux-gnu]
+
 #Script para consulta de informações sobre domínios nacional/internacional
 #Criado por Daniel Gomes - padulas@hotmail.com
 
@@ -26,12 +27,15 @@ puts "2 - DNS"
 puts "3 - TXT"
 puts "4 - SOA"
 puts "5 - A"
-puts "6 - Scanner TCP/UDP"
+puts "6 - Check Port TCP/UDP"
 puts "7 - Whois Nacional"
 puts "8 - Whois Internacional"
+puts "9 - CVE's consulting"
+puts "10- Check Malware"
 
 #Escolha de opção a ser pesquisada
 puts "Escolha uma opção:"
+
 opcao = gets.chomp.to_i   
 
 if opcao == 1
@@ -47,7 +51,7 @@ elsif opcao == 4
 	system "nslookup -type=#{comandos[3]} #{dominio}"
 
 elsif opcao == 5
-	system "nslookup -type=#{comandos[4]} #{dominio}"
+	system "host #{dominio} |grep address*"
 
 elsif opcao == 6
 	puts "Informe uma porta TCP/UDP"
@@ -58,10 +62,15 @@ elsif opcao == 7
 	#Importante definir a variável "opcao conforme: opcao = "'palavras_entre_aspas_simples dentro de aspas duplas'"
 	system "whois #{dominio} |grep -E #{whoisbr}"
 
-else opcao == 8
+elsif opcao == 8
 	system "whois #{dominio} |grep -E #{whoiscom}"
 
-	
+elsif opcao == 9
+	system "nmap -Pn --script vuln #{dominio}"
+
+elsif opcao == 10
+	system "nmap -sV --script=http-malware-host #{dominio}"
+
 end
 
 #Fim das condições if,elsif,else
