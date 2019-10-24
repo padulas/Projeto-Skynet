cfc334b401a1ddfb94af5443b677b2b652d97c54
padulas <41273121+padulas@users.noreply.github.com>
2019-10-23 21:09:05 -0300

    *************
    Signed-off-by: padulas <41273121+padulas@users.noreply.github.com>

--- skynet.rb
+++ skynet.rb
@@ -1,4 +1,4 @@
-#Versão Inicial Skynet 2019
+#Versão Inicial Skynet 2019 **************************************
 #skynet.rb - Build 1.0
 
 #ruby 2.5.5p157 (2019-03-15 revision 67260) [x86_64-linux-gnu]
