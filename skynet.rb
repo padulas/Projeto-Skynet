e0fc03a2afccb0b4447db34f3c41c333afa635c6
WorkStation-Home <padulas@hotmail.com>
2019-10-27 10:52:18 -0300

    Skynet com case pronto.
    Signed-off-by: WorkStation-Home <padulas@hotmail.com>

--- skynet.rb
+++ skynet.rb
@@ -1,16 +1,13 @@
-1234
 #Versão Inicial Skynet 2019
-
 #ruby 2.5.5p157 (2019-03-15 revision 67260) [x86_64-linux-gnu]
-
 #Script para consulta de informações sobre domínios nacional/internacional
 #Criado por Daniel Gomes - padulas@hotmail.com
 
-#Declarando variáveis fixas:
-#Declarando as variáveis de comandos
+#Declarando as variáveis de "comandos"
 comandos = ['mx', 'ns', 'txt', 'soa', 'a']
 
 #Declarando filtros para uso do "Whois".
+
 whoisbr = "'changed|expires|provider|status'"
 whoiscom = "'Registry Expiry Date:|Domain Name:|Updated Date:|Creation Date:|Registrar:'"
 
@@ -19,79 +16,93 @@ puts "\n"
 puts "Informe um domínio:"
 puts "\n"
 
-#Declarando a variável dominio
+#Declarando a variável "dominio"
 dominio = gets.chomp
-puts ""
+puts "\n"
 
-#Oferecendo opcoes:
+#Exibe opções ao usuário
 puts "Agora escolha uma opção de consulta:"
-puts ""
+puts "\n"
 puts "FERRAMENTAS DNS:"
 puts "1 - MX"
 puts "2 - DNS"
 puts "3 - TXT"
 puts "4 - SOA"
 puts "5 - A"
-puts ""
+puts "\n"
 puts "FERRAMENTAS DE REDE:"
 puts "6 - Check Port TCP/UDP"
 puts "7 - Whois Nacional"
 puts "8 - Whois Internacional"
-puts ""
+puts "\n"
 puts "FERRAMENTAS DE SI"
 puts "9 - CVE's consulting"
 puts "10 - Check Malware"
-puts "11 - Instalar Aplicações"
+puts "11 - checagem de portas"
 
 #Escolha de opção a ser pesquisada
 puts "Escolha uma opção:"
 
+#Declarando a variável "opcao"
 opcao = gets.chomp.to_i   
-puts ""
+puts "\n"
 
-if opcao == 1
+#Opcoes estrutura de controle
+	case opcao
+when 1
 	system "nslookup -type=#{comandos[0]} #{dominio}"
 
-elsif opcao == 2
+when 2
 	system "nslookup -type=#{comandos[1]} #{dominio}"
 
-elsif opcao == 3
+when 3
 	system "nslookup -type=#{comandos[2]} #{dominio}"
 
-elsif opcao == 4
+when 4
 	system "nslookup -type=#{comandos[3]} #{dominio}"
 
-elsif opcao == 5
-	system "host #{dominio} |grep address*"
+when 5
+	system 'host #{dominio} |grep address*'
 
-elsif opcao == 6
-	puts "Informe uma porta TCP/UDP"
-	portas = gets.chomp.to_i
+when 6
+	"Informe uma porta TCP/UDP"
+end
+
+#Declarando variável "portas da opcao "8""
+portas = gets.chomp.to_i
 	system "nmap -p #{portas} #{dominio}"
 
-elsif opcao == 7
-	
-#Importante definir a variável "opcao conforme: opcao = "'palavras_entre_aspas_simples dentro de aspas duplas'"
+	case opcao 
+
+when 7
+
+#OBS: Importante definir a variável "opcao conforme: opcao = "'palavras_entre_aspas_simples dentro de aspas duplas'"
 	
 	system "whois #{dominio} |grep -E #{whoisbr}"
 
-elsif opcao == 8
+when 8
 	system "whois #{dominio} |grep -E #{whoiscom}"
 
-elsif opcao == 9
+when 9
 	system "nmap -Pn --script vuln #{dominio}"
 
-elsif opcao == 10
+when 10
 	system "nmap -sV --script=http-malware-host #{dominio}"
 
-elsif opcao == 11
-	system "yum install nmap"
-
+when 11
+	system "Instalar ferramentas"
 end
 
-#Fim das condições if,elsif,else
+#Fim de estrutura condicional
+"Consulta concluida!!!"
+"Compartilhe o conhecimento!!!"
+puts "\n"
 
-puts "Consulta concluida!!!"
-puts "Compartilhe o conhecimento!!!"
+"OBS: Caso a opcao 9-10 apresente erro instale o NMAP com os seguintes comandos:"
+"Distribuição Debian e derivados:"
+"#apt update -y ; apt install nmap -y"
+
+"Distribuição Redhat Fedora e derivados"
+"###############################################################################"
 
 #Fim de código
