0990ec82fac57d0c1cdc4a4a64418ccd44343cbe
WorkStation-Home <padulas@hotmail.com>
2019-10-27 03:27:22 -0300

    Arquivos do curso pronto. Aula 3
    Signed-off-by: WorkStation-Home <padulas@hotmail.com>

---
+++ loop.rb
@@ -0,0 +1,17 @@
+
+Default
+count = 1
+loop do 
+  puts count
+  break if count == 10
+  # Incrementa a variável count
+  count += 1
+end
+	
+count = 1
+loop do 
+  puts count
+  break if count == 10
+  # Incrementa a variável count
+  count += 1
+end
