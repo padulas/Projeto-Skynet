88d7e7d49b625a656b19fcc0bab0e25281dcb312
padulas <41273121+padulas@users.noreply.github.com>
2019-10-28 19:18:06 -0300

    Calculadora inicial
    Signed-off-by: padulas <41273121+padulas@users.noreply.github.com>

---
+++ calculadora.rb
@@ -0,0 +1,49 @@
+
+#Calculadora:
+#Declarando variáveis - N1 e N2
+puts "Sua mais nova calculadora em Ruby"
+puts "Informe o primeiro número"
+
+#Entrada de dados
+#Declarando variáveis:
+
+#Declarando vaviável 1
+N1 = gets.chomp
+puts "Informe um segundo número"
+
+#Declarando variável 2
+N2 = gets.chomp
+
+puts "\n"
+puts "Selecione qual operação a ser feita"
+puts "\n"
+puts "1 - Soma"
+puts "2 - Subtração"
+puts "3 - Multiplicação"
+puts "4 - Divisão"
+puts "5 - Sair do programa.
+
+puts "Selecione uma opção"
+gets.chomp
+
+case opcao
+
+when 1
+    N1 + N2
+N1 + N2
+
+when 2
+    N1 - N2
+
+when 3
+    N1 * N2
+    
+when 4
+    N1 / N2
+
+when 5
+    exit
+
+case 
+
+    
