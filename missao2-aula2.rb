03b5c27ca6e27a40fb75d302d01f861f09bb82b5
WorkStation-Home <padulas@hotmail.com>
2019-10-30 02:07:53 -0300

    Missão 2 - Aula 2
    Signed-off-by: WorkStation-Home <padulas@hotmail.com>

---
+++ missao2-aula2.rb
@@ -0,0 +1,14 @@
+#Missão 2 - Aula 2
+#Crie um programa que receba o nome e idade de uma pessoa e no final exiba estes dois dados em uma única frase.
+
+puts "Informe seu primeiro nome"
+puts "Informe seu sobrenome"
+
+nome = gets.chomp_to.s
+sobrenome = gets.chomp_to.s
+
+puts "Seu nome compreto é: #{nome} + #{sobrenome}.
+
+
+
+
