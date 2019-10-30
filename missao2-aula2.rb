bf9f0433b654d5f924970895ac91e645ce088eba
WorkStation-Home <padulas@hotmail.com>
2019-10-30 03:01:03 -0300

    Missao 3 aula 2
    Signed-off-by: WorkStation-Home <padulas@hotmail.com>

--- missao2-aula2.rb
+++ missao2-aula2.rb
@@ -2,12 +2,18 @@
 #Crie um programa que receba o nome e idade de uma pessoa e no final exiba estes dois dados em uma única frase.
 
 puts "Informe seu primeiro nome"
+nome = gets.chomp
+
 puts "Informe seu sobrenome"
+sobrenome = gets.chomp
+            
+puts "Informe sua idade"
+idade = gets.chomp
+puts "\n"
+puts "Seu nome completo é: #{nome} #{sobrenome}."
 
-nome = gets.chomp_to.s
-sobrenome = gets.chomp_to.s
+puts "Sua idade é de: #{idade} anos"
 
-puts "Seu nome compreto é: #{nome} + #{sobrenome}.
 
 
 
---
+++ missao3-aula2.rb
@@ -0,0 +1,26 @@
+#Missão 3 - Aula 2
+# Crie um programa que que receba dois números inteiros e no final exiba a soma, subtração, adição e divisão entre eles.
+
+puts "Calculadora 4 operações"
+
+puts "Informe o primeiro número"
+n1 = gets.chomp
+puts "Informe o segundo número"
+n2 = gets.chomp
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+puts "\n"
+
+puts "Resultado da soma: #{soma}"
+puts "Resultado da subtracao: #{subtracao}"
+puts "Resultado da multiplicacao: #{multiplicacao}"
+puts "Resultado da divisao: #{divisao}"
+
+
+
+
+
