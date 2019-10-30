ad162109b2a063b1352d64731a9a931cd391adb6
WorkStation-Home <padulas@hotmail.com>
2019-10-30 02:00:31 -0300

    Calculadora opções
    Signed-off-by: WorkStation-Home <padulas@hotmail.com>

--- calculadora.rb
+++ calculadora.rb
@@ -23,13 +23,12 @@ puts "3 - Multiplicação"
 puts "4 - Divisão"
 puts "5 - Sair do programa"
 puts "\n"
-puts "Escolha uma opcao"
+puts "Escolha uma opcao:"
 puts "\n"
 
 opcao = gets.chomp.to_i
 
 case opcao
-
 when 1
     resultado = n1 + n2
     
@@ -45,6 +44,8 @@ when 4
 when 5
     exit
 end
+
+puts "\n"
 
 puts "E o resultado de seu cálculo é: #{resultado}"
 
